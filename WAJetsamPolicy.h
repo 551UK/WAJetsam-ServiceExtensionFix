d11d01f70e0d53addc7ecbb7f6e0072f10318a2d
551UK <wdf@hotmail.co.uk>
2026-09-06 14:14:09 +0100

    40 MB Jetsam policy helpers

---
+++ WAJetsamPolicy.h
@@ -0,0 +1,40 @@
+#ifndef WAJ_POLICY_H
+#define WAJ_POLICY_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#define WAJ_TARGET_LIMIT_MB 40
+
+static inline bool waj_is_whatsapp_service_path(const char *path) {
+    if (!path) return false;
+
+    static const char *suffixes[] = {
+        "/WhatsApp.app/PlugIns/ServiceExtension.appex/ServiceExtension",
+        "/WhatsApp Business.app/PlugIns/ServiceExtension.appex/ServiceExtension",
+        "/WhatsAppBusiness.app/PlugIns/ServiceExtension.appex/ServiceExtension",
+        "/Watusi.app/PlugIns/ServiceExtension.appex/ServiceExtension"
+    };
+
+    size_t length = strlen(path);
+    for (size_t i = 0; i < sizeof(suffixes) / sizeof(suffixes[0]); i++) {
+        size_t suffix_length = strlen(suffixes[i]);
+        if (length >= suffix_length &&
+            strcmp(path + length - suffix_length, suffixes[i]) == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static inline int32_t waj_clamp_positive_limit(int32_t value) {
+    return value > 0 && value < WAJ_TARGET_LIMIT_MB ? WAJ_TARGET_LIMIT_MB : value;
+}
+
+static inline uint32_t waj_clamp_flags_limit(uint32_t value) {
+    return value > 0 && value < WAJ_TARGET_LIMIT_MB ? WAJ_TARGET_LIMIT_MB : value;
+}
+
+#endif
